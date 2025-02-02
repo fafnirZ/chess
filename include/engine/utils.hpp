4420d82ec7e15cd88759a9cbc6810b3c51408004
fafnir <gokurohono@gmail.com>
2025-02-02 21:48:43 +1100

    feat: logic for writing reverse deep copy of unique ptr array logic

--- include/engine/utils.hpp
+++ include/engine/utils.hpp
@@ -5,8 +5,7 @@ namespace Utils {
 
 template <typename T>
 std::vector<std::unique_ptr<T>> reverse_unique_ptr_deep_copy_vals(
-    const std::vector<std::unique_ptr<T>>& original
-);
+    const std::vector<std::unique_ptr<T>>& original);
 
 }
 }
